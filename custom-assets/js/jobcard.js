278ee997f6a6e74427d056c00d985fc620085ca8
Shantanu Phatke <sphatkes@gmail.com>
2020-04-21 10:44:38 +0530

    Dynamic entry of vehicle information and condition in job card

--- custom-assets/js/jobcard.js
+++ custom-assets/js/jobcard.js
@@ -23,10 +23,36 @@ const getData = () => {
 
 const fillJobCard = jobData => {
 
+    // Vehicle info
     const vehicleNumber = document.getElementById("input-vehicle-number");
     vehicleNumber.value = jobData.vehicle_number
 
     const vehicleBrand = document.getElementById("input-vehicle-brand");
     vehicleBrand.value = jobData.vehicle_brand
 
+    const vehicleKm = document.getElementById("input-vehicle-km");
+    vehicleKm.value = jobData.km
+
+    const vehicleFuel = document.getElementById("input-vehicle-fuel");
+    vehicleFuel.value = jobData.fuel
+
+
+    // Vehicle condition
+    const headlights = document.getElementById("input-headlights");
+    headlights.value = jobData.vehicle_condition.headlights
+
+    const tiers = document.getElementById("input-tiers");
+    tiers.value = jobData.vehicle_condition.tiers
+
+    const breaks = document.getElementById("input-breaks");
+    breaks.value = jobData.vehicle_condition.breaks
+
+    const horn = document.getElementById("input-horn");
+    horn.value = jobData.vehicle_condition.horn
+
+    const airbags = document.getElementById("input-airbags");
+    airbags.value = jobData.vehicle_condition.airbags
+
+    const mirrors = document.getElementById("input-mirrors");
+    mirrors.value = jobData.vehicle_condition.mirrors
 }
