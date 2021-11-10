8f8ca9a127796dc925f92536b414ebeba103912b
shantanuphatke <sphatkes@gmail.com>
2021-11-10 23:55:43 +0530

    removed firebase-config details

--- custom-assets/js/firebase-config.js
+++ custom-assets/js/firebase-config.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCAhIphiUqGJAIqhpSmjDq5goBYc543rCQ",
-  authDomain: "garage-management-8bf37.firebaseapp.com",
-  databaseURL: "https://garage-management-8bf37.firebaseio.com",
-  projectId: "garage-management-8bf37",
-  storageBucket: "garage-management-8bf37.appspot.com",
-  messagingSenderId: "517041804101",
-  appId: "1:517041804101:web:5caff135c4e3068cc4a42c",
-};
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+}
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
